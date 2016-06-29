153aeb5db2eda46de904d682f9d1cfc9048e558f
Daniel H <frontend@reddesignsystems.com>
2016-06-29 10:08:02 -0500

    Add passport and strategy passport-local - set midlewares in express

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express')
 const cookieParse = require('cookie-parser') /* parsear cookies*/
 const bodyParser = require('body-parser') /* parsear body de http request*/
 const expressSession = require('express-session') /* maneja las sesiones para persistir a users registrado*/
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const port = process.env.PORT || 8080
 
 const app = express()
@@ -14,10 +16,13 @@ app.use(bodyParser.json()) /* parsear bodys que puedan llegar en json*/
 app.use(bodyParser.urlencoded({ extended: false })) /* parsear bodys que puedan llegar encodeados en la url como forms*/
 app.use(cookieParse()) /* parsear cookies que llegan a la app*/
 app.use(expressSession({
-	secret: 'my secret are mine' /* sessiones trabajan con una llave secreta para encriptar la sesion*/
+	secret: 'my secret are mine', /* sessiones trabajan con una llave secreta para encriptar la sesion*/
 	resave: false,
 	saveUninitialized: false
 }))
+
+app.use(passport.initialize()) /*decirle a express que trabaje con passport*/
+app.use(passport.session()) /* utilizar sesiones que tiene passport*/
 app.use(express.static(path.join(__dirname, 'public')))
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
